914ffec1178ef60855969137acfb7c7a7454d2f0
Kieren Diment <zarquon@cpan.org>
2019-08-21 08:39:54 +1000

    move parse_iso routine from test into module

--- lib/Musical/Note.pm
+++ lib/Musical/Note.pm
@@ -28,8 +28,76 @@ with a more OO and pluggable interface.
 
 my @scale = ('c', 'c#', 'd', 'd#', 'e', 'f',  'f#', 'g',  'g#',  'a',  'a#',  'b');
 
+around BUILDARGS => sub {
+    my ($orig, $class, @args) = @_;
+    if (@args == 1 && ! ref @args) {
+        my $init = $args[0];
+        if (looks_like_number $init) {
+            return { midinum => $init };
+        }
+        elsif ( $init =~ /a-g/i) {
+            return $class->_parse_iso($init);
+        }
+    }
+    return $args[0] if ref $args[0] eq 'HASH';
+    return +{ @args };
+};
 
+sub _parse_iso {
+    my ($class, $t) = @_;
 
+    my ($step, @tokens) = split '', $t;
+    if (! looks_like_number ($tokens[-1])) {
+        push @tokens, 4;
+    }
+    my ($alter, $octave);
+
+    my $get_octave = sub {
+        my $o = shift @tokens;
+        if ($o eq '-') {
+            $o .= shift @tokens;
+            $o += 0;
+        }
+        return $o;
+    };
+
+    my $next = $tokens[0];
+    if (looks_like_number($next) || $next eq '-' ) {
+        $alter = 0;
+        $octave = $get_octave->();
+        return { step => $step, octave => $octave, alter => $alter };
+    }
+
+    my $p = {
+        'b' => sub {
+            $next = shift @tokens;
+            my $peek = $tokens[0];
+            if ($peek eq 'b') {
+                $alter = -2;
+                shift @tokens;
+            }
+            else {
+                $alter = -1;
+            }
+            $octave = $get_octave->();
+            return { step => $step, octave => $octave, alter => $alter };
+        },
+        '#' => sub {
+            $next = shift @tokens;
+            $octave = $get_octave->();
+            return { step => $step, octave => $octave, alter => 1 };
+        },
+        'x' => sub {
+            $next = shift @tokens;
+            $octave = $get_octave->();
+            return { step => $step, octave => $octave, alter => 2 };
+        },
+    };
+
+    return $p->{$next}->();
+}
+
+    
 has _scale => (
     is => 'lazy',
     default => sub {
