6bdcf382aac1151d1168e82753cae2137a02bfd6
Kieren Diment <zarquon@cpan.org>
2019-12-16 17:37:23 +1100

    musical scale implementation now works

--- lib/Musical/Scale.pm
+++ lib/Musical/Scale.pm
@@ -6,172 +6,63 @@ use List::Util;
 use Carp;
 use Array::Circular;
 
-around BUILDARGS => sub {
-    my ($orig, $class, @args) = @_;
-    my @fixed_args;
-    my %info;
+use Moo;
 
-    # hashref it's already been looked up, pass it through.
-    # bit more thought required here :)
+# extends 'Musical::Note';
 
-    # single string, or a hash
-    if ( ! ref $args[0] ) {
-        # - string it's a name look it up.
-        if (@args == 1 && ! ref $args[0]) {
-            %info  = %{ Musical::Scale::List->new->scale_for($args[0]) };
-        }
-        else {
-            %info = @args;
-        }
+has root => (
+    is => 'ro',
+    coerce => sub {
+        my ($r) = @_;
+        return $r if ref $r && $r->isa('Musical::Note');
+        return Musical::Note->new($r);
     }
-    else { # hashref constructor
-        %info = %{ $args[0] };
-    }
-    
-    my $default = List::Util::any { exists $info{$_} }
-        qw/interval_nums interval_names note_nums/;
+);
 
-    # "At least one of 'interval_nums', 'interval_names' or 'note_nums'
-    # must be supplied to constructor"
+has current => (
+    is => 'rwp',
+    default => sub {
+    },
+);
 
-    return $class->$orig(\%info);
-};
-
-has name => (
+has mode => (
     is => 'ro',
-    required => 0,
-);
-
-has interval_nums => (
-    is => 'ro',
-    required => 0,
-);
-
-has note_nums => (
-    is => 'ro',
-    required => 0
-);
-
-has interval_names => (
-    is => 'ro',
-    required => 0
-);
-
-has start_octave => (
-    is => 'lazy',
-    default => sub { 4 }
-);
-
-has key => (
-    is => 'lazy',
-    default => sub { 'C' },
+    default => 'Major'
 );
 
 has intervals => (
-    is => 'ro',
-    lazy => 1,
-    default => sub {
-        my $i =  Array::Circular->new(@{$_[0]->interval_nums});
-        $i->loops($_[0]->start_octave);
-        return $i;
-    }
-);
-
-has scale => (
-    is => 'ro',
-    lazy => 1,
+    is => 'lazy',
     default => sub {
         my ($self) = @_;
-        my @scale;
-        my $iv = $self->intervals;
-        my $note = Musical::Note->new($self->key . $self->start_octave);
-        push @scale, $note;
-        for ( 1 .. @{$self->intervals} ) {
-            $note = $note->transpose($self->intervals->current);
-            $self->intervals->next;
-            push @scale, $note;
-        }
-        my $scale = Array::Circular->new(@scale);
-        $DB::single = 1;
-        return $scale;
-    }
+        return Musical::Scale::List->new->scale_for($self->mode)->{interval_nums};
+    },
 );
 
-
-has current_note => (
-    is => 'rw',
-    default => sub {
-        my ($self) = @_;
-        my $name = $self->key . $self->start_octave;
-        return Musical::Note->new($name);
-    }
+has octaves => (
+    is => 'ro',
+    default => 1
 );
 
-sub next_note {
-    my ($self, $steps) = @_;
-    $steps ||= 1;
-    my $interval = 0;
-    $interval += $self->intervals->next for ( 1 .. $steps);
-    my $note = $self->_note_for($interval);
-    return $note;
+sub up {
+    my ($self) = @_;
+    $self->_set_current($self->root) unless $self->current;
+    my $c = $self->current;
+    my $n = $self->current->transpose( $self->intervals->current_and_next);
+    $self->_set_current($n);
+    return $c;
 }
 
-sub prev_note {
-    my ($self, $steps) = @_;
-    $steps ||= 1;
-    my $interval = 0;
-    $interval -= $self->intervals->previous for ( 1 .. $steps);
-    my $note = $self->_note_for($interval);
-    return $note;
+sub down {
+    my ($self) = @_;
+    $self->_set_current($self->root) unless $self->current;
+    my $c = $self->current;
+    my $n = $self->current->transpose( 0 - $self->intervals->current_and_next);
+    $self->_set_current($n);
+    return $self->current;
 }
 
-*previous_note = \&prev_note;
 
-sub _note_for {
-    my ($self, $interval) = @_;
-    my $new = $self->current_note->transpose($interval);
-    return $new;
-}
-
-sub _current_and_step {
-    my ($self, $interval, $steps) = @_;
-    $steps ||= 1;
-    my $current = $self->current_note;
-    my $new = $self->_note_for($interval);
-    my $direction;
-    if ($interval > 0) {
-        $direction = 'next';
-    }
-    elsif ($interval < 0) {
-        $direction = 'previous';
-    }
-    if ($direction) {
-        $self->intervals->$direction for 1 .. $steps;
-    }
-    return $new;
-}
-
-sub _calc_interval {
-    my ($self, $steps, $dir) = @_;
-    my $other_dir = $dir eq 'next' ? 'previous' : 'next';
-    my $interval = 0;
-    $interval = $interval + $self->intervals->$dir for 1 .. $steps;
-    $self->intervals->$other_dir for 1 .. $steps; # reset back to where we were
-    $interval = - $interval if $dir eq 'previous';
-    return $interval;
-}
-sub note_and_next {
-    my ($self, $steps) = @_;
-    $steps ||=1;
-    my $interval = $self->_calc_interval($steps, 'next');
-    return $self->_current_and_step($interval, $steps);
-}
-
-sub note_and_previous {
-    my ($self, $steps) = @_;
-    $steps ||=1;
-    my $interval = $self->_calc_interval($steps, 'previous');
-    return $self->_current_and_step($interval, $steps);
-}
 
 1;
+
+
--- t/090-scales.t
+++ t/090-scales.t
@@ -2,23 +2,46 @@
 use warnings;
 use strict;
 use Musical::Note;
+use Musical::Scale;
 use Musical::Scale::List;
+use Array::Circular;
 use Test::More;
+
 my @cmaj1 = (Musical::Note->new('C4'));
-
 my @i = qw(2 2 1 2 2 2 1);
-push @cmaj1, $cmaj1[-1]->transpose($_) for @i;
+my $exp;
 
-my @exp = ('C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5');
+subtest 'naive implementation of scale' => sub {
 
+    push @cmaj1, $cmaj1[-1]->transpose($_) for @i;
+    my @exp = ('C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5');
+    my @cmaj2 = (Musical::Note->new('C4'));
+    my @i2 = @{ Musical::Scale::List->new->scale_for('Major')->{interval_nums} };
+    is_deeply(\@i, \@i2, "intervals match");
+    push @cmaj2, $cmaj2[-1]->transpose($_) for @i2;
+    my $got = [ map { $_->iso } @cmaj2 ];
+    is_deeply $got, \@exp, "C major scale as we expected";
+    $exp = [ map { $_->iso } @cmaj1  ];
+    is_deeply( $got, $exp, "generated from intervals") ;
+};
 
-my @cmaj2 = (Musical::Note->new('C4'));
+subtest 'implementation with Array::Circular' => sub {
+    my $i = Musical::Scale::List->new->scale_for('Major')->{interval_nums};
+    my @c = (Musical::Note->new('C4'));
+    push @c, $c[-1]->transpose($i->current_and_next) for ( 1 .. @$i);
+    is_deeply($exp,  [ map { $_->iso } @c ], 'intervals as Array::Circular');
+};
 
-my @i2 = @{ Musical::Scale::List->new->scale_for('Major')->{interval_nums} };
-is_deeply(\@i, \@i2, "intervals match");
-push @cmaj2, $cmaj2[-1]->transpose($_) for @i2;
-my $got = [ map { $_->iso } @cmaj2 ];
-is_deeply $got, \@exp, "C major scale as we expected";
-my $exp = [ map { $_->iso } @cmaj1  ];
-is_deeply( $got, $exp, "generated from intervals") ;
+subtest 'proper oo impl' => sub {
+    my $scale = Musical::Scale->new(root => 'C4');
+    my @s = map { $scale->up } 0 .. @{$scale->intervals};
+    my $got = [map { $_->iso } @s];
+
+    is_deeply $exp, $got, 'oo implementation generates correct scale';
+    my $rev_exp = [reverse @$exp];
+    my @sd = map { $scale->down->iso } 0 .. @{$scale->intervals};
+    diag explain \@sd ;
+    is_deeply $rev_exp, \@sd, "Descending also generates correct scale";
+};
+
 done_testing;
